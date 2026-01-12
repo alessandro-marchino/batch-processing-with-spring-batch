fc00319590cdbcc46e80f68543ed2c8ccbbb6596
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-01-12 12:43:48 +0100

    Lesson 12

--- spring-batch/src/main/java/com/infybuzz/springbatch/config/SampleJob.java
+++ spring-batch/src/main/java/com/infybuzz/springbatch/config/SampleJob.java
@@ -13,20 +13,25 @@ import org.springframework.batch.infrastructure.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class SampleJob {
 
+	private final JobRepository jobRepository;
+
 	@Bean
-	Job fistJob(JobRepository jobRepository) {
+	Job fistJob() {
 		return new JobBuilder("First job", jobRepository)
-			.start(firstStep(jobRepository))
+			.start(firstStep())
+			.next(secondStep())
 			.build();
 	}
 
-	private Step firstStep(JobRepository jobRepository) {
+	private Step firstStep() {
 		return new StepBuilder("First step", jobRepository)
 			.tasklet(firstTask())
 			.build();
@@ -37,6 +42,21 @@ public class SampleJob {
 			@Override
 			public @Nullable RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 				log.info("This is the first tasklet step");
+				return RepeatStatus.FINISHED;
+			}
+		};
+	}
+
+	private Step secondStep() {
+		return new StepBuilder("Second step", jobRepository)
+			.tasklet(secondTask())
+			.build();
+	}
+	private Tasklet secondTask() {
+		return new Tasklet() {
+			@Override
+			public @Nullable RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				log.info("This is the second tasklet step");
 				return RepeatStatus.FINISHED;
 			}
 		};
