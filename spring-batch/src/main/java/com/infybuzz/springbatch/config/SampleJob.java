edba36af28f947dda8876bf912770538ef7c9338
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-01-12 14:07:51 +0100

    Lesson 16

--- spring-batch/src/main/java/com/infybuzz/springbatch/config/SampleJob.java
+++ spring-batch/src/main/java/com/infybuzz/springbatch/config/SampleJob.java
@@ -1,15 +1,10 @@
 package com.infybuzz.springbatch.config;
 
-import org.jspecify.annotations.Nullable;
 import org.springframework.batch.core.job.Job;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.Step;
-import org.springframework.batch.core.step.StepContribution;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.infrastructure.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
